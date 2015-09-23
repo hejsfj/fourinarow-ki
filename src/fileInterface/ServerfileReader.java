eb2d2b84b5bfe81f16892e415814bae0443fb5cf
marcel <marce@141.72.137.129>
2015-09-23 16:39:22 +0200

    serverfileReader
    - wird mit player initialisiert um dateinamen zu bestimmen
    - pollt nun auf das Verzeichnis im Abstand von 300ms
    - refactoring

--- src/fileInterface/ServerfileReader.java
+++ src/fileInterface/ServerfileReader.java
@@ -14,36 +14,50 @@ import org.xml.sax.SAXException;
 
 public class ServerfileReader {
 	private String sharedFolderPath;
-	private String player;
+	private char player;
+	Serverfile serverFile;	
 	
-	public ServerfileReader(String sharedFolderPath, String player){
+	public ServerfileReader(String sharedFolderPath, char player){
 		this.sharedFolderPath = sharedFolderPath;
 		this.player = player;
 	}
 	
-	public Serverfile readServerfile(){		
-		Serverfile serverFile = new Serverfile();		
-	
-		File xmlFile = new File(sharedFolderPath + "/server2spielerx.xml");
-//		File xmlFile = new File(sharedFolderPath + "/server2spielero.xml");
+	public Serverfile readServerfile(){			
+		File xmlFile = new File(sharedFolderPath + "/server2spieler" + player + ".xml");
 		
+		while (!xmlFile.exists()){
+			try {
+				Thread.sleep(300);
+				System.out.println("wartet auf XML von Server");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		try {
-			Document xmlDoc = getDocFromXmlFile(xmlFile);
-			Node contentNode = getNodeFromXmlDoc(xmlDoc, "content");	
-			Element element = (Element) contentNode;		
-
-			boolean freigegeben = Boolean.parseBoolean(element.getElementsByTagName("freigabe").item(0).getTextContent());
-			
-			serverFile.setFreigabe(freigegeben);				
-			serverFile.setSatzstatus(element.getElementsByTagName("satzstatus").item(0).getTextContent());				
-			serverFile.setGegnerzug(Integer.parseInt(element.getElementsByTagName("gegnerzug").item(0).getTextContent()));				
-			serverFile.setSieger(element.getElementsByTagName("sieger").item(0).getTextContent());
+			serverFile = new Serverfile();
+			setServerfileValuesFromXML(xmlFile);			
+			xmlFile.delete();
 		}
 		catch (Exception e) { 
 			System.out.println("Error:");
-			e.getMessage();
-		}		
+			System.out.println(e.getMessage());
+		}
 		return serverFile;	
+	}
+
+	private void setServerfileValuesFromXML(File xmlFile) throws ParserConfigurationException, SAXException, IOException {
+		Document xmlDoc = getDocFromXmlFile(xmlFile);
+		Node contentNode = getNodeFromXmlDoc(xmlDoc, "content");	
+		Element element = (Element) contentNode;		
+
+		boolean freigegeben = Boolean.parseBoolean(element.getElementsByTagName("freigabe").item(0).getTextContent());
+		
+		serverFile.setFreigabe(freigegeben);				
+		serverFile.setSatzstatus(element.getElementsByTagName("satzstatus").item(0).getTextContent());				
+		serverFile.setGegnerzug(Integer.parseInt(element.getElementsByTagName("gegnerzug").item(0).getTextContent()));				
+		serverFile.setSieger(element.getElementsByTagName("sieger").item(0).getTextContent());
+		
 	}
 
 	private Node getNodeFromXmlDoc(Document xmlDoc, String node) {
