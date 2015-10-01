12c0dc3ffe57ca736d803a8334b439f3e0e004e5
marcel <marce@141.72.137.129>
2015-10-01 13:05:12 +0200

    Spiel über FileSchnittstelle läuft

--- src/application/GameController.java
+++ src/application/GameController.java
@@ -1,9 +1,20 @@
 package application;
 
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import com.pusher.client.channel.PrivateChannel;
+
+import agentKI.AgentKI;
+import fileInterface.Agentfile;
+import fileInterface.AgentfileWriter;
+import fileInterface.Serverfile;
+import fileInterface.ServerfileReader;
+import gamefield.Gamefield;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -13,22 +24,23 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import pusherInterface.PusherConnector;
+import pusherInterface.PusherEvent;
+import pusherInterface.PusherEventHandler;
 
 
 public class GameController implements Initializable {
 	
-    @FXML //  fx:id="myButton"
-    private Button newButton; // Value injected by FXMLLoader
-    
-    @FXML
-    private Button statsButton; // Value injected by FXMLLoader
-    
-	@FXML
-    private Button startButton;
+    @FXML private Button newButton; // Value injected by FXMLLoader    
+    @FXML private Button statsButton; // Value injected by FXMLLoader    
+	@FXML private Button startButton;
 	
-	@FXML
-    private Label infostat;
+	@FXML private Label infostat;
+	@FXML private GridPane gamefieldGrid;
+	
+	GameProperties gameProperties;
 
 	@Override
 	public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
@@ -37,50 +49,34 @@ public class GameController implements Initializable {
 		assert startButton != null : "fx:id=\"statsButton\" was not injected: check your FXML file";
 		
         newButton.setOnAction(new EventHandler<ActionEvent>() {
-
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Load it!");
-          
-                Stage stage; 
-  
-                   //get reference to the button's stage         
-                   stage=(Stage)newButton.getScene().getWindow();
-                   //load up OTHER FXML document
-                   
-                	AnchorPane page;
-					try {
-						page = (AnchorPane) FXMLLoader.load(getClass().getResource("Start.fxml"));
-						//create a new scene with root and set the stage
-		                 Scene scene = new Scene(page);
-		                 stage.setScene(scene);
-		                 stage.show();
-					
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}    
+	            System.out.println("Load it!");
+	      
+	            Stage stage; 
+	            stage=(Stage)newButton.getScene().getWindow();
+               
+            	AnchorPane page;
+				try {
+					page = (AnchorPane) FXMLLoader.load(getClass().getResource("Start.fxml"));
+	                stage.setScene(new Scene(page));
+	                stage.show();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}    
             }    
         });
 		
 		statsButton.setOnAction(new EventHandler<ActionEvent>() {
-
 			@Override
 			public void handle(ActionEvent event) {
 				System.out.println("Zum neuen Spiel");
-
 				Stage stage;
-
-				// get reference to the button's stage
 				stage = (Stage) statsButton.getScene().getWindow();
-				// load up OTHER FXML document
-
 				AnchorPane page;
 				try {
 					page = (AnchorPane) FXMLLoader.load(getClass().getResource("Stats.fxml"));
-					// create a new scene with root and set the stage
-					Scene scene = new Scene(page);
-					stage.setScene(scene);
+					stage.setScene(new Scene(page));
 					stage.show();
 
 				} catch (IOException e) {
@@ -94,31 +90,119 @@ public class GameController implements Initializable {
 
 			@Override
 			public void handle(ActionEvent event) {
-				System.out.println("Zum neuen Spiel");
+				System.out.println("Zum neuen Spiel");			
+				infostat.setText("Das Spiel beginnt");				
 				
-				infostat.setText("Das Spiel beginnt");
-//				Stage stage;
-
-//				// get reference to the button's stage
-//				stage = (Stage) startButton.getScene().getWindow();
-//				// load up OTHER FXML document
-//
-//				AnchorPane page;
-//				try {
-//					page = (AnchorPane) FXMLLoader.load(getClass().getResource("Start.fxml"));
-//					// create a new scene with root and set the stage
-//					Scene scene = new Scene(page);
-//					stage.setScene(scene);
-//					stage.show();
-//
-//				} catch (IOException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
+				gameProperties = new GameProperties();
+				readProperties();
+				String usedInterface = gameProperties.getProperty(GameProperties.INTERFACE);
+				
+				if (usedInterface.equals("File")){
+					startFileInterfaceGame();
+				}
+				if (usedInterface.equals("Push")){
+					startPusherInterfaceGame();
+				}
 			}
-		});//endSetOnActionStartButton
-		
+
+			private void startPusherInterfaceGame() {
+				System.out.println("PusherInterface not implemented yet");
+				Gamefield gamefield = new Gamefield();
+				AgentKI agent = new AgentKI();
+				char player = gameProperties.getProperty(GameProperties.SPIELER).charAt(0);
+				String appKey = gameProperties.getProperty(GameProperties.APP_KEY);
+				String appSecret = gameProperties.getProperty(GameProperties.APP_SECRET);
+				
+				PusherConnector pusher = new PusherConnector("", appKey, appSecret);
+				PusherEventHandler eventHandler = new PusherEventHandler();
+				PrivateChannel channel = pusher.subscribeToPrivateChannel("private-channel", eventHandler, "MoveToAgent");
+				PusherEvent pusherEvent;
+
+				int eventCounter = 0;
+				while (true) {
+					if (eventHandler.hasEvent) {
+						
+						pusherEvent = eventHandler.getPusherEvent();
+						eventHandler.hasEvent = false;
+						
+						eventCounter++;
+						
+						System.out.println("EventCounter: " + eventCounter);
+						
+						
+						System.out.println("Gegnerzug: " + pusherEvent.getGegnerzug());
+						
+						if (pusherEvent.getGegnerzug() != -1 && pusherEvent.getGegnerzug() != -2){
+							gamefield.insertCoin(gamefieldGrid, pusherEvent.getGegnerzug(), (player == 'o') ? 'x' : 'o');
+						}
+						if (!pusherEvent.isGameOver()){
+							
+							int move = agent.calculateMove(gamefield);
+							System.out.println("calculated move: " + move);
+							gamefield.insertCoin(gamefieldGrid, move, player);
+							pusher.triggerClientMove(channel, move);
+							
+						} else {
+							System.out.println("Spiel vorbei. Der Gewinner ist: " + pusherEvent.getSieger());
+							break;
+						}
+					}
+				}				
+			}
+
+			private void startFileInterfaceGame() {				
+
+				AgentKI agent = new AgentKI();
+
+				Gamefield gamefield = new Gamefield();
+				String sharedFolderPath = gameProperties.getProperty(GameProperties.DATEIPFAD);
+				
+				char player = gameProperties.getProperty(GameProperties.SPIELER).charAt(0);
+				AgentfileWriter agentfileWriter = new AgentfileWriter(sharedFolderPath, player);
+				ServerfileReader serverfileReader = new ServerfileReader(sharedFolderPath, player);	
+
+				while (true) {
+					try {
+						Serverfile serverfile = serverfileReader.readServerfile();
+						
+						if (serverfile.getGegnerzug() != -1){
+							gamefield.insertCoin(gamefieldGrid, serverfile.getGegnerzug(), (player == 'o') ? 'x' : 'o');
+						}
+						if (!serverfile.isGameOver()){
+							int move = agent.calculateMove(gamefield);
+							gamefield.insertCoin(gamefieldGrid, move, player);
+							agentfileWriter.writeAgentfile(new Agentfile(move));
+						} else {
+							System.out.println("Spiel vorbei. Der Gewinner ist: " + serverfile.getSieger());
+							break;
+						}
+					} catch (final Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
 	}
-   
+	
+	private void readProperties() {
+		InputStream input = null;
+    	
+    	try {
+    		input = new FileInputStream(GameProperties.DATEINAME);
+    		gameProperties.load(input);
+    	} catch (IOException ex) {
+    		ex.printStackTrace();
+        } finally{
+        	if(input!=null){
+        		try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+        	}
+        }
+    }
+	
+	
 }
 
