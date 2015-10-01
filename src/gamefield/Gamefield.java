44ffabbce198702fb11bbf3392f5f73723f7eb2d
marcel <marce@141.72.137.129>
2015-10-01 13:06:28 +0200

    Gamefield updatet auch UI

--- src/gamefield/Gamefield.java
+++ src/gamefield/Gamefield.java
@@ -1,19 +1,15 @@
 package gamefield;
 
+import javafx.geometry.HPos;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
 public class Gamefield {
 	private char[][] gamefield;
 	private final int numRows = 6;
 	private final int numColumns = 7;
 	private boolean isEmpty = true;
-	
-	public static void main(final String[] args) {
-		Gamefield gamefield = new Gamefield();
-		gamefield.insertCoin(3, 'o');
-		gamefield.insertCoin(3, 'o');
-		gamefield.insertCoin(3, 'o');
-		gamefield.insertCoin(3, 'o');
-		gamefield.insertCoin(3, 'o');
-	}
 	
 	public Gamefield(){
 		gamefield = new char[numRows][numColumns];
@@ -28,13 +24,15 @@ public class Gamefield {
 	     }
 	}
 	
-	public void insertCoin(int columnIndex, char player) {
+	public void insertCoin(GridPane gridPane, int columnIndex, char player) {
 		
-		for (int rowIndex = numRows - 1; rowIndex>=0; rowIndex--)
+		for (int rowIndex = numRows - 1; rowIndex >= 0; rowIndex--)
 		{
 			if (gamefield[rowIndex][columnIndex] == ' ' )
 			{
 				gamefield[rowIndex][columnIndex] = player;
+
+				addNewCoinToGridPane(gridPane, columnIndex, rowIndex, player);
 				if (isEmpty) { isEmpty = false;}
 				break;
 			}
@@ -49,5 +47,44 @@ public class Gamefield {
 	}
 	private boolean isColumnFull(int columnIndex){
 		 return gamefield[0][columnIndex] != ' ';
+	}
+	private void addNewCoinToGridPane(GridPane pane, int columnIndex, int rowIndex, char player){
+		Circle newCircle = getPlayerCircle(player);
+		pane.add(newCircle, columnIndex, rowIndex);
+		GridPane.setHalignment(newCircle, HPos.CENTER);
+	}
+	
+	private Circle getPlayerCircle(char player){
+		return new Circle(22.5, getColorForPlayer(player));
+		
+	}
+	
+	private Color getColorForPlayer(char player){
+		if (player == 'o')
+			return getSpielerOColor();
+		else if(player == 'x')
+			return getSpielerXColor();
+		else 
+			return new Color(1.0, 1.0, 1.0, 1.0);
+			
+	}
+	
+	private Color getSpielerXColor(){
+		final int[] rgbValues = getRGBValuesFromColorHexCode("ff3d00");	   
+	    return Color.rgb(rgbValues[0],rgbValues[1],rgbValues[2]);
+	}	
+	
+	private Color getSpielerOColor(){
+		final int[] rgbValues = getRGBValuesFromColorHexCode("fff500");	   
+	    return Color.rgb(rgbValues[0],rgbValues[1],rgbValues[2]);
+	}	
+	
+	private int[] getRGBValuesFromColorHexCode(String colorHexCode){
+		final int[] ret = new int[3];
+	    for (int i = 0; i < 3; i++)
+	    {
+	        ret[i] = Integer.parseInt(colorHexCode.substring(i * 2, i * 2 + 2), 16);
+	    }
+	    return ret;		
 	}
 }
