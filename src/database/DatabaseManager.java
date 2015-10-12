e62a550e26a553428961c80794a756231bed497d
marcel <marce@141.72.137.129>
2015-10-12 16:18:23 +0200

    Datenbank-Code bisschen umstrukturiert

--- src/database/DatabaseManager.java
+++ src/database/DatabaseManager.java
@@ -5,24 +5,20 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 
 import database.DatabaseStructure;
 
 public class DatabaseManager {
 	
 	Connection connection = null;
-	DatabaseStructure DatabaseStructure;
 
 	
 	public DatabaseManager(){
-		DatabaseStructure = new DatabaseStructure();
 		databaseConnect();
 		
 		try {
 			initializeDatabase();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	
@@ -40,7 +36,7 @@ public class DatabaseManager {
 	    } 
 	  	  
 	    try{ 
-	      connection = DriverManager.getConnection(DatabaseStructure.path, DatabaseStructure.user, DatabaseStructure.password); 
+	      connection = DriverManager.getConnection(DatabaseStructure.PATH, DatabaseStructure.USER, DatabaseStructure.PASSWORD); 
 	      
 	    } catch ( SQLException e ) { 
 	      e.printStackTrace(); 
@@ -50,9 +46,9 @@ public class DatabaseManager {
 	
 	public void initializeDatabase() throws SQLException{
 		
-		execute(DatabaseStructure.createTableSpiel);
-		execute(DatabaseStructure.createTableSatz);
-		execute(DatabaseStructure.createTableZug);
+		execute(DatabaseStructure.CREATE_TABLE_SPIEL);
+		execute(DatabaseStructure.CREATE_TABLE_SATZ);
+		execute(DatabaseStructure.CREATE_TABLE_ZUG);
 		
 	}
 	
@@ -87,27 +83,27 @@ public class DatabaseManager {
     }
 
 
-	public void addspiel(int spiel_id, String spielerO, String spielerY, String sieger, String datum) throws SQLException{
+	public void addSpiel(int spiel_id, String spielerO, String spielerY, String sieger, String datum) throws SQLException{
 			this.execute("INSERT INTO spiel(spielero,spielery,sieger,datum) VALUES('"+spielerY+"','"+spielerO+"','"+sieger+"','"+datum+"')");	 	
 	}
 
-	public void addsatz(int spiel_id, int satz_nr, int punktespielero,int punktespielerx, String startspieler) throws SQLException{
+	public void addSatz(int spiel_id, int satz_nr, int punktespielero,int punktespielerx, String startspieler) throws SQLException{
 		this.execute("INSERT INTO satz(spiel_id, satz_nr, punktespielero, punktespielerx, startspieler)VALUES('"+spiel_id+"','"+satz_nr+"','"+punktespielero+"','"+punktespielerx+"','"+startspieler+"')");
 	}
 	
-	public void addzug(int spiel_id, int satz_nr,int zug_nr, int spalte, String spieler) throws SQLException{
+	public void addZug(int spiel_id, int satz_nr,int zug_nr, int spalte, String spieler) throws SQLException{
 		this.execute("INSERT INTO zug(spiel_id,satz_nr,zug_nr,spalte,spieler) VALUES('"+spiel_id+"','"+satz_nr+"','"+zug_nr+"','"+spalte+"','"+spieler+"')");
 	}
 	
-	public ResultSet getspiele(){
+	public ResultSet getSpiele(){
 		return this.query("SELECT * FROM spiele");		
 	}
 
-	public ResultSet getsaetze(String SpielID){
+	public ResultSet getSaetze(String SpielID){
 		return this.query("SELECT * FROM satz WHERE spiel_id = "+SpielID);		
 	}
 	
-	public ResultSet getzuege(String SpielID, String SatzNR){
+	public ResultSet getZuege(String SpielID, String SatzNR){
 		return this.query("SELECT * FROM zug WHERE spiel_id = "+SpielID+"AND satz_nr = "+SatzNR);
 	}
 	
--- src/database/DatabaseStructure.java
+++ src/database/DatabaseStructure.java
@@ -1,13 +1,13 @@
 package database;
 
-class DatabaseStructure {
+public final class DatabaseStructure {
 	
-	String path = "jdbc:hsqldb:datenbank/fourinarow";
-	String user = "sa";
-	String password = "";
+	public static final String PATH = "jdbc:hsqldb:datenbank/fourinarow";
+	public static final String USER = "sa";
+	public static final String PASSWORD = "";
 		
-	String createTableSpiel = "CREATE TABLE IF NOT EXISTS spiel ( id INTEGER IDENTITY, spielero VARCHAR(256),spielerx VARCHAR(256), sieger VARCHAR(256), datum VARCHAR(256)";
-	String createTableSatz = "CREATE TABLE IF NOT EXISTS satz ( spiel_id INTEGER, satz_nr INTEGER, punktespielero INTEGER, punktespielerx INTEGER, startspieler VARCHAR(256), PRIMARY KEY(spiel_id, satz_nr))";
-	String createTableZug = "CREATE TABLE IF NOT EXISTS zug ( spiel_id INTEGER, satz_nr INTEGER, zug_nr INTEGER, spalte INTEGER, spieler VARCHAR(256), PRIMARY KEY(spiel_id, satz_nr, zug_nr))";
+	public static final String CREATE_TABLE_SPIEL = "CREATE TABLE IF NOT EXISTS spiel ( id INTEGER IDENTITY, spielero VARCHAR(256),spielerx VARCHAR(256), sieger VARCHAR(256), datum VARCHAR(256), PRIMARY_KEY(id))";
+	public static final String CREATE_TABLE_SATZ = "CREATE TABLE IF NOT EXISTS satz ( spiel_id INTEGER, satz_nr INTEGER, punktespielero INTEGER, punktespielerx INTEGER, startspieler VARCHAR(256), PRIMARY KEY(spiel_id, satz_nr))";
+	public static final String CREATE_TABLE_ZUG = "CREATE TABLE IF NOT EXISTS zug ( spiel_id INTEGER, satz_nr INTEGER, zug_nr INTEGER, spalte INTEGER, spieler VARCHAR(256), PRIMARY KEY(spiel_id, satz_nr, zug_nr))";
 	
 }
